3738b52be5e5456f030709273d624c967280d7bd
Daniel Hunt <djhunt@mail.csuchico.edu>
2019-11-20 00:46:43 -0800

    Added Assignment x08

--- part1.r
+++ part1.r
@@ -1,14 +1,4 @@
----
-title: "Music Insights"
-author: "Daniel Hunt"
-output:
-  html_document:
-    df_print: paged
----
-
-First we need tidyverse to handle data importing and wrangling
-
-```{r}
+## ------------------------------------------------------------------------
 library("tidyverse")
 library("dplyr")
 library("tidyr")
@@ -16,10 +6,9 @@ library("readr")
 
 survey <- read_csv("https://raw.githubusercontent.com/introdsci/MusicSurvey/master/music-survey.csv")
 prefernces <- read_csv("https://raw.githubusercontent.com/introdsci/MusicSurvey/master/preferences-survey.csv")
-```
 
-Change the names of columns:
-```{r}
+
+## ------------------------------------------------------------------------
 colnames(survey)[colnames(survey)=="Timestamp"] <- "time_submitted"
 
 colnames(survey)[colnames(survey)=="First, we are going to create a pseudonym for you to keep this survey anonymous (more or less). Which pseudonym generator would you prefer?"] <- "pyseudonym_generator"
@@ -41,35 +30,29 @@ colnames(survey)[colnames(survey)=="Artist"] <- "favorite_song_artist"
 colnames(survey)[colnames(survey)=="Song"] <- "favorite_song"
 
 colnames(survey)[colnames(survey)=="Link to song (on Youtube or Vimeo)"] <- "favorite_song_link"
-```
 
-Create a person table that describes a person within the survey
-```{r}
+
+## ------------------------------------------------------------------------
 Person <- tibble(time_submitted=survey$time_submitted, pyseudonym_generator=survey$pyseudonym_generator, psyeudonym=survey$pseudonym, sex=survey$sex, academic_major=survey$academic_major, academic_level=survey$academic_level, year_born=survey$year_born, instruments=survey$instrument_list)
-```
 
-Create a favorate song table that describes a person's favorite song
-```{r}
+
+## ------------------------------------------------------------------------
 FavoriteSong <- tibble(psyeudonym=survey$pseudonym, artist=survey$favorite_song_artist, song=survey$favorite_song, song_link=survey$favorite_song_link)
-```
 
-Create a ratings table that shows the ratings that people have given the various songs
-```{r}
+
+## ------------------------------------------------------------------------
 temp <- gather(prefernces, key = "artist_song", value = "rating", 3:45)
 Ratings <- tibble(psyeudonym=temp$`What was your pseudonym?`, artist_song=temp$artist_song, rating=temp$rating)
-```
 
-Convert the date submitted to POSIX
-```{r}
+
+## ------------------------------------------------------------------------
 Person$time_submitted <- as.POSIXlt(parse_datetime(Person$time_submitted, format="%m/%d/%y %H:%M"))
-```
 
-Convert variables that should be factors, so that R recognizes them as factors
-```{r}
+
+## ------------------------------------------------------------------------
 Person$pyseudonym_generator <- as.factor(Person$pyseudonym_generator)
 Person$sex <- as.factor(Person$sex)
 Person$academic_level <- as.factor(Person$academic_level)
 Person$academic_major <- as.factor(Person$academic_major)
 levels(Person$academic_major)[levels(Person$academic_major)=="Computer information systems"] <- "Computer Information Systems"
-```
 
